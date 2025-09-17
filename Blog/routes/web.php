64a319b5f2b4f4ba0ceeb38c4036fc723d0a87dd
Sebastian Chacon B <87223451+SebastianCHB@users.noreply.github.com>
2025-09-17 15:00:52 -0700

    Users Controller
    Creation of users Controller

--- Blog/routes/web.php
+++ Blog/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsersController;
+
 //ENDPOINTS
 Route::get('/', function () {
     return view('welcome');
@@ -14,6 +16,11 @@ Route::get("/post",function(){
 Route::get("/about",function(){
     return view('about');
 });
-Route::get("/dashboard",function(){
-    return view('admin.dashboard');
+
+Route::group(['prefix'=>'dashboard'],function(){
+    Route::get("/",function(){
+        return view('admin.dashboard');
+    });
+    Route::get("/users",[UsersController::class,'getUsers']);
 });
+
